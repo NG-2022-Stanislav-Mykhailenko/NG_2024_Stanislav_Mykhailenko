98eb242ee3b3711d328d377948ef6692cff22f76
Stanislav Mykhailenko <mykhailenkostanislavzno@gmail.com>
2024-06-12 11:30:25 +0300

    Fix Lesson 1 Task 3

--- Lesson_1/Task_3/main.cpp
+++ Lesson_1/Task_3/main.cpp
@@ -10,11 +10,23 @@ int main()
 
     if (money < 1000)
         cout << "Work more. ";
+
+    if (money > 999)
+        if (money < 1001) {
+            cout << "Illegal salary." << endl;
+            return 1;
+        }
+
     if (money > 1000) {
         if (money > 1000000)
             cout << "You are a millionaire. ";
         if (money < 1000000)
             cout << "You work great. ";
+        if (money > 999999)
+            if (money < 1000001) {
+                cout << "Illegal salary." << endl;
+                return 1;
+            }
     }
 
     cout << "You are doing great!" << endl;
